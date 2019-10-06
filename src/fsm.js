9c8be39ef042282a1f7210cd0dcefb44ab374df4
Aliaksandr Razhachkin <bubonik.san1993@gmail.com>
2019-10-07 00:19:23 +0300

    50% task complete

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,64 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+      this.state = config.initial;
+      this.config = config;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+      return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+      if (this.config.states.hasOwnProperty(state)) {this.state = state;}
+      else throw new Error();
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+      if (!this.config.states[this.state].transitions.hasOwnProperty(event)) {
+        throw new Error();
+      }
+
+      switch (event) {
+
+        case 'study' :
+          this.changeState('busy');
+          break;
+
+        case 'get_tired' :
+          this.changeState('sleeping');
+          break;
+
+        case 'get_hungry' :
+          this.changeState('hungry');
+          break;
+
+        case 'eat' :
+        case 'get up' :
+          this.changeState('normal');
+          break;
+      }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+      this.state = this.config.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,14 +68,33 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event = null) {
+      let array = [];
+        for(let key in this.config.states){
+          array.push(key);
+        }
+      if (!event) {
+        return array;
+      }
+      else {
+        array = [];
+        for(let key in this.config.states) {
+          if (this.config.states[key].transitions.hasOwnProperty(event)) {
+            array.push(key);
+          }
+        }
+        return array;
+      }
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+
+    }
 
     /**
      * Goes redo to state.
